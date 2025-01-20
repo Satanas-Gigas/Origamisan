ec7640797ed96ccc5a4686c3af896b5baeaab9c0
Satanas-Gigas <robot-tyaga@yandex.ru>
2025-01-20 22:06:14 +0500

    rabotaem_nad_designom

--- blog/templatetags/custom_filters.py
+++ blog/templatetags/custom_filters.py
@@ -1,7 +1,16 @@
 from django import template
+import random
 
 register = template.Library()
 
 @register.filter(name='add_class')
 def add_class(value, class_name):
     return value.as_widget(attrs={'class': class_name})
+
+@register.filter
+def shuffle(value):
+    """Перемешивает список."""
+    if isinstance(value, list):
+        random.shuffle(value)
+        return value
+    return value
